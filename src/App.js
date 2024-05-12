67bc2d15ffbee0fbac9dd0d886d8538d98ab6236
Chetan <chetan@osiansoftware.com>
2024-05-12 11:12:08 +0530

    modal pattern

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Modal } from "./components/Modal";
+
+function App() {
+  return (
+    <>
+      <Modal>
+        <p>
+          I'm baby hammock PBR&B polaroid marxism. Everyday carry la croix
+          forage, green juice mlkshk authentic fashion axe messenger bag pabst
+          tumblr chartreuse ugh 8-bit glossier brunch. Fanny pack DSA cliche
+          intelligentsia bushwick forage cupping humblebrag +1 asymmetrical
+          bitters lyft mixtape yes plz. Normcore stumptown hammock +1
+          thundercats. Ramps vegan thundercats actually pabst, butcher selvage
+          skateboard succulents man bun bushwick aesthetic photo booth portland.
+          Twee mlkshk food truck dreamcatcher freegan. Banh mi gentrify swag,
+          pok pok tattooed edison bulb chambray echo park.
+        </p>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
