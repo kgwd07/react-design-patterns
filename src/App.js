9e3339e20db29cd84ed980e0996163680d875b37
Chetan <chetan@osiansoftware.com>
2025-03-03 07:26:10 +0530

    added render method in data source

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import { BookInfo } from "./components/book-info";
 import { DataSource } from "./components/data-source";
 import { UserInfo } from "./components/user-info";
+import { CurrentUserLoader } from "./components/Current-user-loader";
+import { UserLoader } from "./components/user-loader";
+import { ResouceLoader } from "./components/resource-loader";
 
 const fetchData = async (url) => {
   const response = await axios.get(url);
@@ -9,7 +12,7 @@ const fetchData = async (url) => {
 };
 
 const getDataFromLocalStorage = (key) => () => {
-  return localStorage.getItem(key);
+  return Promise.resolve(localStorage.getItem(key)); // AS WE ARE USING AWAIT INSIDE THE DataSource COMPONENT
 };
 
 const Message = ({ msg }) => <h1>{msg}</h1>;
@@ -17,13 +20,33 @@ const Message = ({ msg }) => <h1>{msg}</h1>;
 function App() {
   return (
     <>
-      <DataSource getData={() => fetchData("/users/1")} resourceName={"user"}>
+      {/* <CurrentUserLoader>
+          <UserInfo/>
+      </CurrentUserLoader> */}
+
+      {/* <UserLoader userId={"3"}>
+          <UserInfo/>
+      </UserLoader> */}
+
+      {/* <ResouceLoader resouceUrl={"/users/2"} resourceName={"user"}>
+      <UserInfo/>
+      </ResouceLoader> */}
+
+      {/* <ResouceLoader resouceUrl={"/books/1"} resourceName={"book"}>
+        <BookInfo/>
+      </ResouceLoader> */}
+
+
+      {/* <DataSource getData={() => fetchData("/users/1")} resourceName={"user"}>
         <UserInfo />
       </DataSource>
 
+      <DataSourceWithRender getData={() => fetchData("/users/1")} resourceName={"user"} render={(resource) => <UserInfo user={resource}/>}>
+      </DataSourceWithRender>
+
       <DataSource getData={() => getDataFromLocalStorage("test")} resourceName={"msg"}>
         <Message />
-      </DataSource>
+      </DataSource> */}
 
     </>
   );
