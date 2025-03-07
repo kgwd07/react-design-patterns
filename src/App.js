8c51ab1fbb2545016f96cd0b73d8b65669ea343e
Chetan <chetan@osiansoftware.com>
2025-03-07 07:52:51 +0530

    custom hook

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { UserInfo } from "./components/user-info";
+
+function App() {
+  return (
+    <>
+      {/* <UserInfo /> */}
+      <UserInfo userId={"1"}/> 
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/user-info.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useCurrentUser } from "./current-user.hook";
+import { useDataSource } from "./data-source.hook";
+import { useResource } from "./resource.hook";
+import { useUser } from "./user.hook";
+import { useCallback } from "react";
+
+const fetchFromServer = (resourceUrl) => async() => {
+  const response = await axios.get(resourceUrl)
+  return response.data
+}
+
+// const getDataFromLocalStorage = (key) = () => {
+//   return localStorage.getItem(key)
+// }
+
+export const UserInfo = ({userId}) => {
+  console.log("RENDERING")
+  // const user = useCurrentUser();
+  // const user = useUser(userId);
+  // const user = useResource('/users/2');
+  const fetchUser = useCallback(fetchFromServer(`/users/${userId}`),[userId])
+  const user = useDataSource(fetchUser)
+  // const message = useDataSource(getDataFromLocalStorage(`msg`))
+  const { name, age, country, books } = user || {};
+  return user ? (
+    <>
+      <h2>{name}</h2>
+      <p>Age: {age} years</p>
+      <p>Country: {country}</p>
+      <h2>Books</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book}> {book} </li>
+        ))}
+      </ul>
+    </>
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
