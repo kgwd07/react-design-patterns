19598855144c367f31f4232811ac16f9c0d92661
Chetan <chetan@osiansoftware.com>
2025-03-09 09:56:21 +0530

    recursive function

---
+++ src/components/recursive.jsx
@@ -0,0 +1,23 @@
+const isValidObj = (data) => typeof data === "object" && data !== null;
+
+export const Recursive = ({ data }) => {
+  if (!isValidObj(data)) {
+    return <li>{data}</li>;
+  }
+
+  const pairs = Object.entries(data);
+  return (
+    <>
+      {pairs.map(([key, value]) => {
+        return (
+          <li>
+            {key}:
+            <ul>
+              <Recursive data={value} />
+            </ul>
+          </li>
+        );
+      })}
+    </>
+  );
+};
