e97d312eb8c52d3a3ff9bb25b83188ed014ff579
kgwd07 <kgwd07@gmail.com>
2025-03-20 07:03:25 +0530

    optimizedContextApi-after

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Buttons from "./components/buttons";
+import Display from "./components/display";
+import "./style.css";
+import { CartProvider } from "./components/cart-context";
+
+function App() {
+  return (
+    <CartProvider>
+      <Display />
+      <Buttons />
+    </CartProvider>
+  );
+}
+
+export default App;
